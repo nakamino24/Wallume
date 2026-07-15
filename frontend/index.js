ad23247221e9f18ca7d70c5eb4f85a212da5c80c
nakamino24 <faiz.quways@gmail.com>
2026-07-15 21:04:34 +0700

    Fix widget-task-handler

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,11 @@
+require('expo-router/entry');
+import { Platform } from 'react-native';
 import { registerWidgetTaskHandler } from 'react-native-android-widget';
 import { widgetTaskHandler } from './src/widgets/widget-task-handler';
 
-registerWidgetTaskHandler(widgetTaskHandler);
+if (Platform.OS === 'android') {
+  registerWidgetTaskHandler(widgetTaskHandler);
+}
 
 // eslint-disable-next-line import/no-unresolved
-require('expo-router/entry');
+
